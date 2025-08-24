85864798dda20ff1928d957bb88fd58878f5ae9b
lourimi <ayoub.lourimi@issatso.u-sousse.tn>
2025-08-24 01:44:52 +0100

    Update page.tsx

--- inventory-app/app/products/page.tsx
+++ inventory-app/app/products/page.tsx
@@ -33,7 +33,7 @@ export default function InventoryPage() {
       return;
     }
 
-    const response = await fetch("http://localhost:5000/products", {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
